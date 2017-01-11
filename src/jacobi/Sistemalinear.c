83b48bb36d3e286938f6be64a19df7222d147be6
Marinofull <Marinofull@users.noreply.github.com>
2017-01-11 03:47:38 -0300

    adaptacao

--- src/jacobi/Sistemalinear.c
+++ src/jacobi/Sistemalinear.c
@@ -1,7 +1,6 @@
 // UNIVERSIDADE FEDERAL DA BAHIA
 // MAT174 - CALCULO NUMERICO
 // PROFESSORA: RITA DE CASSIA NOVAES BARRETO
-// ALUNO: SAULO RIBEIRO DE ANDRADE
 
 // TRABALHO PRATICO 2016.2 - PARTE 1
 
@@ -11,70 +10,161 @@
 
 #include "Sistemalinear.h"
 
-int initializeMatriz(Matriz *m)
+int initializeA(ma *m)
 {
     /* Inicializa a matriz */
-    m->tam_matriz = 0;
+    m->tam = 0;
     return OK;
 }
 
-int append(Matriz *m, float linha[], int tamlinha)
+int initializeB(mb *m)
+{
+    /* Inicializa a matriz */
+    m->tam = 0;
+    return OK;
+}
+
+int appendA(ma *m, float linha[], int tamlinha)
 {
     /* Insere um array de elementos na matriz */
-    int li = m->tam_matriz;
+    int li = m->tam;
     int pos;
 
     for (pos = 0; pos < tamlinha; pos++)
-    {
         m->chave[li][pos] = linha[pos];
-    }
-    m->tam_matriz++;
+
+    m->tam++;
 
     return OK;
 }
 
-int swap(Matriz *m, int indice1, int indice2)
+int appendB(mb *m, float linha[], int tamlinha)
 {
-    /* Troca os elementos da linha indice1 pelos elementos da linha indice2 */
+    /* Insere um array de elementos na matriz */
+    int li = m->tam;
     int pos;
-    int temp;
-    for (pos = 0; pos < MAXSIZE; pos++)
-    {
-        temp = m->chave[indice1][pos];
-        m->chave[indice1][pos] = m->chave[indice2][pos];
-        m->chave[indice2][pos] = temp;
-    }
+
+    for (pos = 0; pos < tamlinha; pos++)
+        m->chave[li] = linha[pos];
+
+    m->tam++;
+
     return OK;
 }
 
+//int swap(Matriz *m, int indice1, int indice2)
+//{
+//    /* Troca os elementos da linha indice1 pelos elementos da linha indice2 */
+//    int pos;
+//    int temp;
+//    for (pos = 0; pos < MAXSIZE; pos++)
+//    {
+//        temp = m->chave[indice1][pos];
+//        m->chave[indice1][pos] = m->chave[indice2][pos];
+//        m->chave[indice2][pos] = temp;
+//    }
+//    return OK;
+//}
+
+int ler(ma *a, mb *b, char *fileA, char *fileB){
+    FILE *fpa, *fpb;                    // Ponteiro do arquivo de entrada
+
+    char linha[255];             // String que guarda a ultima linha lida do arquivo
+    char linha_ant[255];         // String que guarda a penultima linha lida do arquivo
+
+    float linha_matriz[MAXSIZE]; // Linha dos inteiros extraidos do arquivo de entrada
+    int tam_linha_matriz = 0;  // Guarda o tamanho da linha lida do arquivo de entrada
+
+    //lê a matriz A
+    int pos;
+    for (pos = 0; pos < MAXSIZE; pos++)
+        linha_matriz[pos] = 0;
+
+    fpa = fopen(fileA, "r"); // Abra o arquivo de entrada
+
+    if (!fpa)
+    {
+        // Verifique se o nome do arquivo corresponde a algum arquivo existente em disco
+        // Se o arquivo nao existir, interrompa a execucao do programa
+        printf("Erro: Arquivo de entrada nao existe.\n");
+        return FAIL;
+    }
+    else
+    {
+        fgets(linha, 255, fpa);                                        // Leia uma linha do arquivo
+        while (strcmp(linha, linha_ant))                               // Verifique se essa linha ja foi processada (chegou no fim do arquivo)
+        {
+            ha(linha, linha_matriz, &tam_linha_matriz);
+
+            appendA(a, linha_matriz, tam_linha_matriz); // Insira os elementos extraidos na matriz
+            strcpy(linha_ant, linha);                                  // Coloque essa linha como ja processada
+            fgets(linha, 255, fpa);                                     // Leia a proxima linha
+        }
+        fclose(fpa);                                                    // Feche o arquivo
+    }
+
+    //lê o vetor b
+    tam_linha_matriz = 0;
+    for (pos = 0; pos < MAXSIZE; pos++)
+        linha_matriz[pos] = 0;
+
+    fpb = fopen(fileB, "r"); // Abra o arquivo de entrada
+
+    if (!fpb)
+    {
+        printf("Erro: Arquivo de entrada nao existe.\n");
+        return FAIL;
+    }
+    else
+    {
+        fgets(linha, 255, fpa);                                        // Leia uma linha do arquivo
+        while (strcmp(linha, linha_ant))                               // Verifique se essa linha ja foi processada (chegou no fim do arquivo)
+        {
+            ha(linha, linha_matriz, &tam_linha_matriz);
+
+            appendB(b, linha_matriz, tam_linha_matriz); // Insira os elementos extraidos na matriz B
+            strcpy(linha_ant, linha);                                  // Coloque essa linha como ja processada
+            fgets(linha, 255, fpa);                                     // Leia a proxima linha
+        }
+        fclose(fpb);                                                    // Feche o arquivo
+    }
+
+    return OK;
+}
+
+int ha(char linha[], float linha_matriz[], int *tam_linha_matriz){
+    char *pedaco;                // String para guardar os numeros contidos nas linhas do arquivo
+    float num;                   // Inteiro para guardar a string pedaco apos conversao
+    int pos = 0;                 // Indice temporario para o array linha_matriz
+
+    pedaco = strtok(linha, " ");                               // Extraia um dos numeros da linha usando essa funcao
+    while (pedaco)                                             // Repita ate processar todos os numeros da linha
+    {
+        num = atof(pedaco);                                    // Converta a string lida em um inteiro
+        linha_matriz[pos] = num;                               // Atribua a sua posicao correta na matriz
+        pos++;                                                 // Incremente o indice de coluna
+        pedaco = strtok(NULL, " ");                            // Extraia outro numero
+    }
+    *tam_linha_matriz = pos;                                   // Salve a quantidade de elementos extraidos da linha do arquivo
+
+    return OK;
+}
 
 //int gaussJacobi(A, b) {
-int jacobi(Matriz *m, float erro, int iteracoes, float solucao[]){
-    var x = new Array();
-    for (var k = 0; k < m.tam_matriz; k++)
-    {
-        solucao[k] = 0; //Math.floor((Math.random() * 10000) + 1);
+int jacobi(ma *a, mb *b, float erro, int iteracoes, float solucao[]){
+}
+
+void testeai(ma *a, mb *b){
+    int i, j;
+
+    for(i=0; i< a->tam; i++){
+        for(j=0; j< a->tam; j++)
+            printf("%f ", a->chave[i][j]);
+        printf("\n");
     }
 
-    var ni = 0;//Contador de iterações
-    var continuar = true;
-
-    while (continuar && ni < iteracoes) {
-        for (var i=0; i < b.length; i++) {
-            soma = 0;
-            for (var j = 0; j < b.length; j++) {
-                if (j != i) {
-                    soma = soma + A[i][j]*solucao[j]/A[i][i];
-                }
-                x[i] = (b[i]/A[i][i]) - soma;
-            }
-        }
-        if (Math.abs(norm(x) - norm(solucao)) < erro) {
-            continuar = false;
-        } else {
-            solucao=x.slice(0);
-        }
-        ni = ni + 1;
+    for(i=0; i< b->tam; i++){
+            printf("%f ", b->chave[i]);
+        printf("\n");
     }
-    return solucao;
 }
--- src/jacobi/Sistemalinear.h
+++ src/jacobi/Sistemalinear.h
@@ -1,7 +1,6 @@
 // UNIVERSIDADE FEDERAL DA BAHIA
 // MAT174 - CALCULO NUMERICO
 // PROFESSORA: RITA DE CASSIA NOVAES BARRETO
-// ALUNO: SAULO RIBEIRO DE ANDRADE
 
 // TRABALHO PRATICO 2016.2 - PARTE 1
 
@@ -19,15 +18,26 @@
 #define OK 0       // Retorno do programa em caso de exito
 #define MAXSIZE 50 // Tamanho maximo da matriz
 
-typedef struct listamatriz
+typedef struct ma
 {
     float chave[MAXSIZE][MAXSIZE];
-    int tam_matriz;
-} Matriz;
+    int tam;
+} ma;
 
-int initializeMatriz(Matriz *m);
-int append(Matriz *m, float linha[], int tamlinha);
-int swap(Matriz *m, int num_linha1, int num_linha2);
-int jacobi(Matriz *m, float erro, int iteracoes, float solucao[]);
+typedef struct mb
+{
+    float chave[MAXSIZE];
+    int tam;
+} mb;
+
+int initializeA(ma *m);
+int initializeB(mb *m);
+int appendA(ma *m, float linha[], int tamlinha);
+int appendB(mb *m, float linha[], int tamlinha);
+//int swap(Matriz *m, int num_linha1, int num_linha2);
+int ler(ma *a, mb *b, char *fileA, char *fileB);
+int ha(char linha[], float linha_matriz[], int *tam_linha_matriz);
+int jacobi(ma *a, mb *b, float erro, int iteracoes, float solucao[]);
+void testeai(ma *a, mb *b);
 
 #endif // SISTEMALINEAR_H
